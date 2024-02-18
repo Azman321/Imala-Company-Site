8ee1e92c9bac58dd084eea1af818fbae586b3dac
Romario Reid <rdot.rr@gmail.com>
2024-02-18 23:11:12 +0000

    created a skilss section, a contact section and a projects section made some change to app.css and app.js

---
+++ src/components/Projects.js
@@ -0,0 +1,55 @@
+import { Container, Row, Col, } from "react-bootstrap";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "/Users/romarioreid/imala/Imala-Company-Site/src/assets/img/project-img3.png"
+import colorSharp2 from "../assets/img/color-sharp2.png";
+
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
